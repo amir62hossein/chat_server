ed5ce177bb5987081999262e68c4818da4a07888
amir62hossein <amirnourian378@gmail.com>
2023-02-18 15:28:03 +0330

    single chat added

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ const app = express();
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+const users = [];
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
+  console.log(`user: ${userId} connected`);
   const socketId = socket.id;
-  console.log(userId);
+  users.push({
+    userId,
+    socketId,
+  });
+
+  socket.on("send-message", (message) => {
+    const filteredUser = users.filter((elem) => elem.userId === message.to);
+    const receiverSocketId = filteredUser[0].socketId;
+    socket.broadcast
+      .to(receiverSocketId)
+      .emit("onMessage", { message: message.message, from: userId });
+  });
 
   socket.on("disconnect", (event) => {
-    console.log("userId: " + userId + "disconnect");
+    console.log("userId: " + userId + "   disconnect");
+    const index = users.indexOf((elem) => elem.userId === userId);
+    users.slice(index, 1);
   });
-  
 });
 
 server.listen(port, () => {
